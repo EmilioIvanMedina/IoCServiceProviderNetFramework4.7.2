275c2302aaa95e80411bbc1d0397c530706485e4
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-08-19 21:29:50 -0300

    Set up the Global.asax class in order to register any service to inject

--- IoCWithServiceProvider/IoCPresentation/Global.asax.cs
+++ IoCWithServiceProvider/IoCPresentation/Global.asax.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using IoCPresentation.App_Start;
+using Microsoft.Extensions.DependencyInjection;
+using SampleServices.Interfaces;
+using SampleServices.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +14,25 @@ namespace IoCPresentation
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            
+            DependencyResolver.SetResolver(new ServiceProviderDependencyResolver(ServiceProvider));
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
         }
     }
 }
