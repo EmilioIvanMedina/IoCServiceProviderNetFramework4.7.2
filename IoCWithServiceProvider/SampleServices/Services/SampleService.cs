3cf11564b25f86e35fccef89b2809065f9c68c20
Emilio Medina Martinez <emilio.medinamartinez@gmail.com>
2024-08-19 21:11:09 -0300

    Adding Service class implementing interface in SampleServices project

---
+++ IoCWithServiceProvider/SampleServices/Services/SampleService.cs
@@ -0,0 +1,17 @@
+﻿using SampleServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleServices.Services
+{
+    public class SampleService : ISampleService
+    {
+        public int GetNumber()
+        {
+            return 150;
+        }
+    }
+}
